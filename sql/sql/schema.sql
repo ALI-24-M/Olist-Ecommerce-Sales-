63def07a544b2a19818e29a28e6437aed2691fba
ALI-24-M <220201878@ostimteknik.edu.tr>
2026-06-22 16:15:35 +0300

    Create customers and orders tables in schema.sql

---
+++ sql/sql/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE customers (
+    customer_id VARCHAR(50) PRIMARY KEY,
+    customer_unique_id VARCHAR(50),
+    customer_zip_code_prefix INT,
+    customer_city VARCHAR(100),
+    customer_state VARCHAR(10)
+);
+
+CREATE TABLE orders (
+    order_id VARCHAR(50) PRIMARY KEY,
+    customer_id VARCHAR(50),
+    order_status VARCHAR(30),
+    order_purchase_timestamp TIMESTAMP,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
